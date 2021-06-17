3d324b79917c8b2ceadd5c27d7ab46d4d28e2544
Kim Frans <kimfrans34@gmail.com>
2021-06-17 16:18:18 +0200

    filtering

--- factory-function/shoe-factory.js
+++ factory-function/shoe-factory.js
@@ -9,7 +9,7 @@ function shoeCatalogue(){
     {shoe:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmcWES6CVPLDm3V6Rd5cjuZQ9eHZW61stztFDeekk5oGHrIRfHTV7DjGelovWgB5z2UDI&usqp=CAU", brand:"adidas", size:"8", color:"yellow", stock:"10", price:"1200"},
     {shoe:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRg7_XbkxY34nzIiOsKyZqI7JP8t0O8G4t2rJaSTghDTuyUvuPH8WT_6ctphMXP0JRI1rA&usqp=CAU", brand:"adidas", size:"2", color:"white", stock:"10", price:"900"},
     {shoe:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwc_h6vhVIYfUeYFEO7a6o5IUilpVrGS1sGxMZoSdtvM9MvcwION-1R13ADTA1mGxQaaY&usqp=CAU", brand:"vans", size:"5", color:"black", stock:"15", price:"900"},
-    {shoe:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyu3qT-P2W9wqeLwhY6KVzVSLoI0u6Wfwigv9yUGJsFBpPRiimjocqHIQuXzpzbHR85iA&usqp=CAU", brand:"vans", size:"9", color:"red", stock:"10", price:"700"},
+    {shoe:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyu3qT-P2W9wqeLwhY6KVzVSLoI0u6Wfwigv9yUGJsFBpPRiimjocqHIQuXzpzbHR85iA&usqp=CAU", brand:"vans", size:"8", color:"red", stock:"10", price:"700"},
     {shoe:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhsXtZvJhIMh13JbTiCfGgfUHYY8AVOhdk1SBEPGipDKXWebFPsnjPnwxROtxsPAwwD0Y&usqp=CAU", brand:"vans", size:"6", color:"yellow", stock:"10", price:"600"},
     {shoe:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNfxVt-Cza-LZ84agnAzUN7J9k9RVWAvYHQyBabD65eSZnbaVbluvfUH5aElwI8Q-_bSE&usqp=CAU", brand:"vans", size:"7", color:"blue", stock:"10", price:"600"},
     {shoe:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQ0qT2Et0qjKsP3jjP_EGOzJy76tqrSzs1fGKzXt7lpQORW5HLGzYrC8EfgM-I7Aaem3o&usqp=CAU", brand:"converse", size:"4", color:"white", stock:"15",price:"600"},
@@ -19,7 +19,21 @@ function shoeCatalogue(){
     {shoe:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcUSBXVOHI-gAtAd3lL33BGRCJO0MM13DVeZN9REjxt0e8z-JpnHmucocxs3ykyE4YClc&usqp=CAU", brand:"jordan", size:"3", color:"white", stock:"15", price:"1400"},
     {shoe:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTA_oFvthz3v4X4cCNjbwRDiAAEqZXiaUH54ynXygsT0hxEA9qH-fkdyrF07s_O6vy8Q5E&usqp=CAU", brand:"jordan", size:"8", color:"black", stock:"10", price:"1300"},
     {shoe:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2ePp1xFTDSOhXLAdu-hoHjbyaoNH8aKpSHrPQjRxDBT7JHb2WBpTgUGo55enrWDmjKIo&usqp=CAU", brand:"jordan", size:"5", color:"grey", stock:"15", price:"1500"},
-    {shoe:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDGILtFg2jC8Yt_NT7GWYamplrCJbTo5ilhYNYVQ-94VftzXcZ1_54BEcpabzf8WpSJj4&usqp=CAU", brand:"jordan", size:"2", color:"pink", stock:"10", price:"1200"}]
+    {shoe:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDGILtFg2jC8Yt_NT7GWYamplrCJbTo5ilhYNYVQ-94VftzXcZ1_54BEcpabzf8WpSJj4&usqp=CAU", brand:"jordan", size:"2", color:"pink", stock:"10", price:"1200"}];
+
+
+    function getShoes(){
+        return shoes
+    }
+
+    function filterShoeBrands(brand, size, color){
+        var shoeFilter = shoes.filter(function (currentElement){
+            return currentElement.brand == brand && currentElement.size == size && currentElement.color == color
+        });
+        console.log(shoeFilter)
+        return shoeFilter
+        
+    }
 
     function values(){
         return{
@@ -29,6 +43,8 @@ function shoeCatalogue(){
     
     return{
         values,
+        getShoes,
+        filterShoeBrands,
     }
 
 }
--- javascript/shoe-cat-DOM.js
+++ javascript/shoe-cat-DOM.js
@@ -1,3 +1,10 @@
+var searchBtn = document.querySelector(".search-button")
+var display = document.querySelector(".display")
+
+var brandSelect = document.getElementById("brand")
+var sizeSelect = document.getElementById("size")
+var colourSelect = document.getElementById("color")
+// console.log(searchBtn)
 var shoeFactory = shoeCatalogue()
 
 var templateSource = document.querySelector(".shoes").innerHTML;
@@ -6,12 +13,48 @@ var templateSource = document.querySelector(".shoes").innerHTML;
 var userTemplate = Handlebars.compile(templateSource);
 
 var templateDisplay = document.querySelector(".shoe-display")
-var userData = {shoe: shoeFactory.values().shoeObj}
+var userData = { shoe: shoeFactory.values().shoeObj }
 
 var userDataHTML = userTemplate(userData)
 templateDisplay.innerHTML = userDataHTML
 
-function myFunction() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
+
+searchBtn.addEventListener("click", function shoeFiltering() {
+
+  var filteredData = { shoe: shoeFactory.filterShoeBrands(brandSelect.value, sizeSelect.value, colourSelect.value) }
+  var filteredDataHTML = userTemplate(filteredData)
+  templateDisplay.innerHTML = filteredDataHTML
+
+
+});
+
+// function myFunction() {
+//   var popup = document.getElementById("myPopup");
+//   popup.classList.toggle("show");
+// }
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("cart-btn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
+}
